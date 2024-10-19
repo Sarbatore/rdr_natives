a5ee7bdadd15a8805a5661dcf93399fb0fcd2a0b
Valentin CHENE <valentin.chene.p@gmail.com>
2024-10-19 14:14:25 +0200

    fixed AttachEntityToCoordsPhysically native name

--- client/entity.lua
+++ client/entity.lua
@@ -15,10 +15,10 @@
 ---@param p13 number
 ---@param p14 number
 function AttachEntityToCoordsPhysically(entity, x, y, z, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14)
-    return Citizen.InvokeNative(0x715012E3A63023C0, entity, x, y, z, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14)
+    return Citizen.InvokeNative(0x445D7D8EA66E373E, entity, x, y, z, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14)
 end
 
----Return the entity which is looting the entity
+---Return the entity which is looting the entity [@sarbatore]
 ---@param entity number
 ---@return number
 function GetEntityLooter(entity)
